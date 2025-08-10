55d960d4175df46a4b0f04c4d79875167db92268
Buciu Petre <36156636+buciupetre2020@users.noreply.github.com>
2025-08-10 17:23:47 +0300

    Modifică structura
    Trecut de la json la csv mai bine structurat

--- main.R
+++ main.R
@@ -1,19 +1,68 @@
 library(httr)
 library(jsonlite)
+library(data.table)
+library(collapse)
 
-bounds <- "13.68,46.94,36.10,55.30"
+proceseaza <- function(statii) {
+  ids <- names(statii)
+  
+  rez <- rbindlist(lapply(seq_along(statii), function(i) {
+    id <- ids[i]
+    statie <- statii[[i]]
+    
+    campuri <- names(statie)[lengths(statie) > 0]
+    
+    dt_camp <- rbindlist(lapply(campuri, function(camp) {
+      if (length(statie[[camp]]) == 0) return(NULL)
+      rbindlist(statie[[camp]], use.names = FALSE) |>
+        fselect(1:2) |>
+        setnames(c("time", "value")) |>
+        fmutate(field = camp)
+    }), use.names = TRUE, fill = TRUE)
+    
+    if (nrow(dt_camp)) dt_camp[, id := id]
+    dt_camp
+  }), use.names = TRUE, fill = TRUE) 
+  
+  return(rez |> 
+    fmutate(across(time:value, \(x) unlist(x))) |> 
+    na_omit() |> roworder(time) |> 
+    fmutate(id = as.integer(id)))
+}
 
-statii <- httr::content(httr::POST("https://api.waqi.info/mapq2/bounds",
+informatii <- function(x){
+rbindlist(x, fill=T) |> 
+  fmutate(geo=unlist(geo)) |> 
+  fmutate(what = rep(c('lat', 'lon'), times=length(x))) |> 
+  dcast(idx + utime + name ~ what, value.var='geo') |>
+  fmutate(idx = as.integer(gsub('[A-Z]+', "", idx))) |>
+  frename(id=idx)
+}
+
+
+
+bounds <- "25.80,44.30,26.35,44.60"
+
+#apel către waqi
+labels <- httr::content(httr::POST("https://api.waqi.info/mapq2/bounds",
                                    body = list(bounds=bounds),
                                    encode = "multipart"))$data
 
-jsonlite::write_json(statii, paste0("data/", "statii", Sys.Date(), ".json"))
+#pune ca csv curățat
+labels <- informatii(labels)
 
-ids <- sapply(statii, function(x) gsub("[A-Z]+", "", x$id))
+fwrite(labels, paste0("data/", "labels", Sys.Date(), ".csv"))
 
-#get station information
-info <- sapply(ids, function(id) content(GET(paste0("https://airnet.waqi.info/airnet/feed/hourly-cached/", id)))$data) 
-write_json(info, paste0("data/", "info", Sys.Date(), ".json"))
+
+#acum măsurătorile stațiilor
+ids <- gsub("[A-Z]+", "", labels$id)
+
+statii <- sapply(ids, function(id) content(GET(paste0("https://airnet.waqi.info/airnet/feed/hourly-cached/", id)))$data) 
+statii <- proceseaza(statii)
+fwrite(statii, paste0("data/", "statii", Sys.Date(), ".csv"))
+
+
+                 
 
 
 
