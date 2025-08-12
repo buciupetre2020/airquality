e5b5e50c2ef91e1bce5cf2e1425ecdbd7831818e
Buciu Petre <36156636+buciupetre2020@users.noreply.github.com>
2025-08-12 18:25:47 +0300

    Update main.R

--- main.R
+++ main.R
@@ -2,6 +2,17 @@ library(httr)
 library(jsonlite)
 library(data.table)
 library(collapse)
+library(ggplot2)
+library(lubridate)
+library(hrbrthemes)
+
+
+#pentru fourier features
+transforma <- function(x, k=24, f=sin) {
+  x <- {as.integer(x)/3600} - min(as.integer(x)/3600) 
+  f(2 * pi * x/k) 
+}
+
 
 proceseaza <- function(statii) {
   ids <- names(statii)
@@ -59,8 +70,51 @@ ids <- gsub("[A-Z]+", "", labels$id)
 
 statii <- sapply(ids, function(id) content(GET(paste0("https://airnet.waqi.info/airnet/feed/hourly-cached/", id)))$data) 
 statii <- proceseaza(statii)
+
+#curatam valori problematice, salvam ultimele 72 ore
+statii <- statii |> 
+  fmutate(time = lubridate::parse_date_time(time, orders='ymdHMS') + lubridate::hours(2)) |> 
+  fgroup_by(field, id) |> roworder(time) |>
+  fmutate(pos = as.integer(time)/3600) |> fungroup() |>
+  fsubset(pos > max(pos) - 72) |> fselect(-pos) |> roworder(field, id, time) |>
+  fgroup_by(field, id) |> 
+  fmutate(const=fnunique(value)) |> 
+  fmutate(crestere = fsd(fgrowth(value))) |> fungroup() |> fsubset(const > 10) |>
+  fgroup_by(field) |> fmutate(categ = fquantile(crestere, 0.1)) |> fungroup() |>
+  fsubset(crestere > categ) |> fselect(-const, -crestere, -categ)
+
+missing <- statii |> fcount(id, field, sort=T) |> 
+  fsubset(N>60) |> fselect(id, field) |> funique()
+
+statii <- join(statii, missing, how='inner') |>
+          roworder(time, field, id)
+                 
 fwrite(statii, paste0("data/statii/", "statii", Sys.Date(), ".csv"))
 
+zile <- list.files('data/statii/*csv)
+zile <- zile[{length(zile)-3}:length(zile)]
+
+statii_last <- rbindlist(lapply(paste0("data/statii/", zile), fread)) |> funique()
+                 
+grafic <- statii_last |> fsubset(field %in% c('met.t', 'met.p', 'met.h', 'pm25', 'co2', 'ch2o')) |>
+  fgroup_by(id, field) |>
+  fmutate(medie = fmean(value)) |>
+  fungroup() |> 
+  ggplot(aes(x=time, y=value, group=id)) + 
+  geom_line() + 
+  geom_smooth(method='lm', aes(x=time, y=value), se=FALSE, colour='red3', linewidth=1,
+            formula = y ~ transforma(x, k=24, f=sin) + transforma(x, k=24, f=cos) + 
+                          transforma(x, k=8, sin) + transforma(x, k=8, cos) +
+                          as.factor(lubridate::day(x)), 
+            inherit.aes = FALSE) + scale_y_log10() +
+  facet_wrap(~field, ncol=3, scales='free') +
+  labs(title = paste0('Poluare București, ', Sys.Date()), x='Oră', y='Valoare') +
+  hrbrthemes::theme_ipsum_es()
+
+#scrie graficul
+png(paste0("data/grafic_", Sys.Date(), ".png"),  res=300, width=3200, height=2000)
+print(grafic)
+dev.off()
 
                  
 
